ba21e21e22f506681612f8113ca1c0633ec72849
Dmitry Maslov <maslow.dmitry2000@ya.ru>
2024-11-09 13:03:23 +0700

    init

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Umidock</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body style="background: linear-gradient(to left, #32414f 0%, #13466b 100%); font-family: Arial">
+    <div style="height: 100vh; width: 100%;display: grid; justify-content: center; align-content: center;  gap: 32px; color: #fff;">
+        <h1 style="font-size: 64px; margin: 0;">Umidock запущен!</h1>
+        <p style="font-size: 32px; text-align: center; margin: 0;">Информация о php ниже ↓</p>
+    </div>
+    <?php phpinfo();?>
+    <style>table {width:100%;max-width:1300px;min-width:900px;}h1,h2 {color:#fff;}</style>
+</body>
+</html>
